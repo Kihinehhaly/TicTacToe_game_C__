81fd466d342d2f7240bb701fb256242c7f06bbf0
petya <test@gmail.com>
2024-05-04 17:13:34 +0300

    Adding game constants and functions-candidates

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+const char X = 'X';
+const char O = 'O';
+const char EMPTY = ' ';
+const char TIE = 'T';
+const char NO_ONE = 'N';
+
+
+void instructions();
+char askYesNo(string question);
+int askNumber(string question, int high, int low = 0);
+char humanPiece();
+char opponent(char piece);
+void displayBoard(const vector<char> &board);
+char winner(const vector<char> &board);
+bool isLegal(const vector<char> &board, int move);
+int humanMove(const vector<char> &board, char human);
+int computerMove(const vector<char> &board, char computer);
+void announceWinner(char winner, char computer, char human);
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
